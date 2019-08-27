bddd6769cdf26ea203546510d589df7e0afb5a12
Michell Ayala <michell.ayala@ksquareinc.com>
2019-08-27 11:57:26 -0500

    add default export

--- src/index.tsx
+++ src/index.tsx
@@ -28,3 +28,21 @@ export {
   TabContainer,
   TabLabel,
 };
+
+export default {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Text,
+  Details,
+  Caption,
+  Header,
+  HeaderTitle,
+  TabItem,
+  ProgressBar,
+  TabContainer,
+  TabLabel,
+};
